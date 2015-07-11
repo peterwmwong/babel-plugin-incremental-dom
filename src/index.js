fc162b87ebec66d46929088d66e27244d39a082e
peterwmwong <peter.wm.wong@gmail.com>
2015-07-11 00:45:52 -0500

    Refactor to use more concise plugin definition

--- src/index.js
+++ src/index.js
@@ -1,48 +1,44 @@
 export default function ({ Plugin, types: t }) {
-  let visitor = {};
+  return new Plugin("incremental-dom", {
+    visitor: {
+      JSXOpeningElement(node, parent, scope, file) {
+        let args = [t.literal(node.name.name)];
+        let key = null;
+        let attrs = [];
 
-  visitor.JSXOpeningElement = {
-    exit(node, parent, scope, file) {
-      let args = [t.literal(node.name.name)];
-      let key = null;
-      let attrs = [];
-
-      for (let attr of node.attributes) {
-        if (attr.name.name === "key") {
-          key = attr.value.value;
-        } else {
-          attrs.push(
-            t.literal(attr.name.name),
-            t.literal(attr.value.value)
-          );
+        for (let attr of node.attributes) {
+          if (attr.name.name === "key") {
+            key = attr.value.value;
+          } else {
+            attrs.push(
+              t.literal(attr.name.name),
+              t.literal(attr.value.value)
+            );
+          }
         }
-      }
 
-      if (key || attrs.length) {
-        args.push(t.literal(key));
-      }
+        if (key || attrs.length) {
+          args.push(t.literal(key));
+        }
 
-      if (attrs.length) {
-        args.push(t.literal(null));
-        args = args.concat(attrs);
-      }
+        if (attrs.length) {
+          args.push(t.literal(null));
+          args = args.concat(attrs);
+        }
 
-      return t.expressionStatement(
-        t.callExpression(t.identifier("elementOpen"), args)
-      );
+        return t.expressionStatement(
+          t.callExpression(t.identifier("elementOpen"), args)
+        );
+      },
+
+      JSXClosingElement(node, parent, scope, file) {
+        return t.expressionStatement(
+          t.callExpression(
+            t.identifier("elementClose"), [
+            t.literal(node.name.name)
+          ])
+        );
+      }
     }
-  };
-
-  visitor.JSXClosingElement = {
-    exit(node, parent, scope, file) {
-      return t.expressionStatement(
-        t.callExpression(
-          t.identifier("elementClose"), [
-          t.literal(node.name.name)
-        ])
-      );
-    }
-  };
-
-  return new Plugin("incremental-dom", { visitor });
+  });
 }
